6eb77cddb048602b90aa263451121a366e605318
Ievgen Zubkov <ievgen.zubkov@intellecteu.com>
2024-06-18 13:50:12 +0300

    Add logout

---
+++ app/logout.tsx
@@ -0,0 +1,3 @@
+export default function Logout() {
+    return <a className="btn btn-secondary" href="/api/auth/logout">Logout</a>;
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import { Button, Col, Container, Row } from "react-bootstrap";
 import Login from "./login";
+import Logout from "./logout";
 
 export default function Home() {
   return (
@@ -7,8 +8,11 @@ export default function Home() {
       <div>
       <Container className="text-center my-5">
             <Row>
-                <Col md={12}>
+                <Col md={6}>
                   <Login></Login>
+                </Col>
+                <Col md={6}>
+                  <Logout></Logout>
                 </Col>
             </Row>
         </Container>
